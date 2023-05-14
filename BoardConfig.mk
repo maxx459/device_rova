689eba9d062194b4c4c973511ce368b17f298326
me-cafebabe <me.cafebabe@gmail.com>
2023-05-15 01:07:06 +0800

    rova: Enable UClamp and exFAT in kernel
    Change-Id: Ic6e5af28bb42e89b8fe56e1a3a28e41b6d0fdab6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,9 @@ TARGET_KERNEL_CONFIG := \
     vendor/xiaomi/msm8937/common.config \
     vendor/xiaomi/msm8937/mi8937.config \
     vendor/xiaomi/feature/android-12.config \
-    vendor/xiaomi/feature/lmkd.config
+    vendor/xiaomi/feature/exfat.config \
+    vendor/xiaomi/feature/lmkd.config \
+    vendor/xiaomi/feature/uclamp.config
 TARGET_KERNEL_VERSION := 4.19
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1

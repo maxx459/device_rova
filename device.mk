cc06895806d4bfeb86c6e215baa1fafb7f3f279a
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-21 23:57:16 +0800

    rova: Goodbye, Lineage Trust HAL
    * Now we set USB restriction through the USB HAL
    
    Change-Id: I4cc69bc982629912a4c716ec9ddea337b12b96b0

--- device.mk
+++ device.mk
@@ -482,10 +482,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_rova
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic

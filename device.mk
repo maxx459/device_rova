a04b74d61e89b3f9067cab7bed030d89dd438554
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-21 23:55:45 +0800

    rova: Switch to USB 1.3-basic HAL
    Change-Id: I6f477db2220b3563b44bb9545b7889166b8a6b06

--- device.mk
+++ device.mk
@@ -488,7 +488,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # Userspace Reboot
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)

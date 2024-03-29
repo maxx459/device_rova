0612350cf69ca479f954d79737db9bdd8b494db0
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-30 02:36:46 +0800

    fixup! rova: Parse modem version string from /sys/devices/soc0/images
    Change-Id: I10336dbaa8211ea3fe7f2e1bfb7b75e24cfa34c5

--- rootdir/bin/init.baseband.sh
+++ rootdir/bin/init.baseband.sh
@@ -2,6 +2,6 @@
 
 baseband_str=$(grep -m1 -az 'QC_IMAGE_VERSION_STRING=' /vendor/firmware_mnt/image/modem.b12 | cut -c 25-)
 
-if [ ! -z $baseband_str ]; then
+if [ "$baseband_str" ]; then
     setprop vendor.gsm.version.baseband $baseband_str
 fi

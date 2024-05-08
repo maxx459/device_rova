377ea80a0b4cad7a729c32a74ac620085a89ea04
maxx 459 <22bq5a4202@vvit.net>
2024-05-08 08:30:15 +0000

    rova: voltage bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rova.mk
+    $(LOCAL_DIR)/voltage_rova.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rova-user \
-    lineage_rova-userdebug \
-    lineage_rova-eng
+    voltage_rova-user \
+    voltage_rova-userdebug \
+    voltage_rova-eng
